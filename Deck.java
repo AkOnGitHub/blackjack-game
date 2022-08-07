a54f86e7371fad186981242e2dc3b00050836a74
Akshay Irudayaraj <akshay.irudayaraj@gmail.com>
2022-08-06 21:39:28 -0700

    fixed deck creation errors, began shuffle()

--- Deck.java
+++ Deck.java
@@ -6,7 +6,20 @@ public class Deck {
         for(Suits suit : Suits.values()) {
             for(Values value : Values.values()) {
                 deck[deckIdx] = new Card(value, suit);
-                deckIdx += 1;
+                
+                deckIdx++;
+                if (deckIdx > 51) break;
+            }
+        }
+    }
+
+    public void shuffle(int times) {
+        for (int i = 0; i < times; i++) {
+            deck[26] = deck[0];
+            for (int j = 25, k = 27; j > 0; j--) {
+                deck[j] = deck[26 - j];
+                deck[k] = deck[j + 1];
+                k--;
             }
         }
     }
